2db6551b36681e4ffa8b4dc326ca03a8d3bb98c5
root <wizzarddguru@gmail.com>
2023-11-17 11:47:08 -0800

    env.c

--- env.c
+++ env.c
@@ -6,24 +6,15 @@
  * Return: A pointer to the environment variables.
  */
 
-char **env_var()
+char **env_var(void)
 {
 	int k;
 	/*char **env = environ;*/
 	char line = '\n';
 
 	for (k = 0; environ[k] != NULL; k++)
-		write(1, environ[k], __strlen(environ[k]));
+		write(1, environ[k], _strlen(environ[k]));
 	write(1, &line, 1);
 
 	return (environ);
-}
-
-void execute_command(char *command, char **env)
-{
-	if (strcmp(command, "env") == 0)
-	{
-		print_environment(env);
-		return;
-	}
 }
